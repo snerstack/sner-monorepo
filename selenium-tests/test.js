25015e4ec3f6368ee6eb55cba229464f45fd5321
filiq <filipker@seznam.cz>
2023-11-06 22:39:53 +0100

    mocha increase timeout

--- selenium-tests/test.js
+++ selenium-tests/test.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-const mocha = new Mocha({ timeout: 10000 })
+const mocha = new Mocha({ timeout: 60000 })
 
 const testFiles = ['home.test.js', 'auth/login.test.js']
 
