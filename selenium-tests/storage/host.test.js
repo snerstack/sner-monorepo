45359c7fd2c482b84b175b7ed2fe06d9dd6e9dce
filiq <filipker@seznam.cz>
2023-11-08 23:00:13 +0100

    selenium host tests

--- selenium-tests/storage/host.test.js
+++ selenium-tests/storage/host.test.js
@@ -1,8 +1,8 @@
 import { expect } from 'chai'
 import { afterEach, beforeEach, describe, it } from 'mocha'
-import { until } from 'selenium-webdriver'
+import { By, until } from 'selenium-webdriver'
 
-import { findTableData } from '../utils/datatables.js'
+import { findTableData, toggleDTToolboxes } from '../utils/datatables.js'
 import { loginUser } from '../utils/loginUser.js'
 import { setupDriver } from '../utils/setupDriver.js'
 
@@ -12,23 +12,62 @@ describe('Host page', () => {
   beforeEach(async () => {
     driver = await setupDriver()
     await loginUser(driver)
+    await driver.get(process.env.FRONTEND_URL + '/storage/host/list')
   })
 
   afterEach(async () => {
     await driver.quit()
   })
 
-  it('views host list', async () => {
-    await driver.get(process.env.FRONTEND_URL + '/storage/host/list')
-
+  it('shows list of hosts', async () => {
     const commentCell = await driver.wait(findTableData('host_list_table', 'a some unknown service server'))
 
     expect(commentCell).to.exist
   })
 
-  it('views host', async () => {
-    await driver.get(process.env.FRONTEND_URL + '/storage/host/list')
+  it('shows more data', async () => {
+    const showMoreButton = await driver.wait(until.elementLocated({ xpath: '//*[@title="Show more data"]' }))
 
+    await showMoreButton.click()
+
+    const showMoreHeadline = await driver.wait(until.elementLocated({ xpath: '//h6[text()="More data"]' }))
+
+    expect(showMoreHeadline).to.exist
+  })
+
+  it('toggle datatables toolbox', async () => {
+    await toggleDTToolboxes(driver)
+
+    const toolbox = await driver.wait(until.elementLocated({ id: 'host_list_table_toolbox' }))
+
+    expect(await toolbox.isDisplayed()).to.be.true
+  })
+
+  it('selects rows', async () => {
+    await toggleDTToolboxes(driver)
+
+    const rows = await driver.wait(
+      until.elementsLocated({ xpath: '//tbody/tr/td[contains(@class, "select-checkbox")]' }),
+    )
+
+    const firstRow = rows[0]
+
+    await firstRow.click()
+
+    let selectedRows = await driver.wait(until.elementsLocated({ xpath: '//tbody/tr[contains(@class, "selected")]' }))
+
+    expect(selectedRows).to.have.lengthOf(1)
+
+    const selectAllButton = await driver.wait(until.elementLocated({ xpath: '//*[@data-testid="host_select_all"]' }))
+
+    await selectAllButton.click()
+
+    selectedRows = await driver.wait(until.elementsLocated({ xpath: '//tbody/tr[contains(@class, "selected")]' }))
+
+    expect(selectedRows).to.have.lengthOf(2)
+  })
+
+  it('views host', async () => {
     const hostLink = await driver.wait(findTableData('host_list_table', '127.4.4.4'))
 
     await hostLink.click()
--- selenium-tests/utils/datatables.js
+++ selenium-tests/utils/datatables.js
@@ -1,5 +1,16 @@
-import { until } from 'selenium-webdriver'
+import { By, until } from 'selenium-webdriver'
 
 export function findTableData(tableId, expectedData) {
   return until.elementLocated({ xpath: `//table[@id="${tableId}"]/tbody/tr/td/*[text()="${expectedData}"]` })
+}
+
+export async function toggleDTToolboxes(driver) {
+  const userNav = await driver.wait(until.elementLocated({ id: 'dropdownUser' }))
+  await userNav.click()
+
+  const toggleDTToolboxesButton = await driver.wait(
+    until.elementLocated({ xpath: '//a[contains(text(), "Toggle DT toolboxes")]' }),
+  )
+
+  await toggleDTToolboxesButton.click()
 }
