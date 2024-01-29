da98b02a5ea2b2e34082f6a34b7a41684e80378e
filiq <kerber@cesnet.cz>
2024-01-29 22:35:10 +0100

    add copy telnet and curl command

--- frontend/src/components/ServiceEndpointDropdown.tsx
+++ frontend/src/components/ServiceEndpointDropdown.tsx
@@ -24,18 +24,44 @@ const ServiceEndpointDropdown = ({
       <div className="dropdown-menu">
         <h6 className="dropdown-header">Service endpoint URIs</h6>
         {getLinksForService(address, hostname, proto, port).map((link) => (
-          <span className="dropdown-item" key={link}>
+          <span className="dropdown-item" key={link.raw}>
             <i
-              className="far fa-clipboard"
-              data-testid="copy-to-clipboard-btn"
-              title="Copy to clipboard"
+              className="far fa-clipboard mr-1"
+              role="button"
+              data-testid="copy-link-to-clipboard-btn"
+              title="Copy link"
               onClick={() => {
-                void navigator.clipboard.writeText(link)
-                toast.info('Copied to clipboard')
+                void navigator.clipboard.writeText(link.raw)
+                toast.info('Link copied to clipboard')
               }}
-            ></i>{' '}
-            <a rel="noreferrer" href={escapeHtml(link)}>
-              {escapeHtml(link)}
+            ></i>
+            <a
+              className="mx-1"
+              role="button"
+              data-testid="copy-telnet-to-clipboard-btn"
+              title="Copy telnet"
+              onClick={() => {
+                void navigator.clipboard.writeText(link.telnet)
+                toast.info('Telnet copied to clipboard')
+              }}
+            >
+              TEL
+            </a>
+            <a
+              className="mr-1"
+              role="button"
+              data-testid="copy-curl-to-clipboard-btn"
+              title="Copy curl"
+              onClick={() => {
+                void navigator.clipboard.writeText(link.curl)
+                toast.info('Curl copied to clipboard')
+              }}
+            >
+              CURL
+            </a>
+            <span className="user-select-none mr-1">|</span>
+            <a rel="noreferrer" href={escapeHtml(link.raw)}>
+              {escapeHtml(link.raw)}
             </a>
           </span>
         ))}
