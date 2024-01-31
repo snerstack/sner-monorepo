e8df2d13c5bd7ceefb600bf04d56d1e8a076a3f8
filiq <kerber@cesnet.cz>
2024-01-31 16:31:16 +0100

    selenium delete confirmation fix

--- server/tests/selenium/__init__.py
+++ server/tests/selenium/__init__.py
@@ -4,6 +4,7 @@ selenium ui tests module
 """
 
 import os
+import time
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -47,6 +48,7 @@ def dt_inrow_delete(sclnt, dt_id, index=0):
     sclnt.find_element(By.ID, dt_id).find_elements(By.XPATH, '//a[@data-testid="delete-btn"]')[index].click()
     webdriver_waituntil(sclnt, EC.alert_is_present())
     sclnt.switch_to.alert.accept()
+    time.sleep(0.25) # timing issue fix
     dt_wait_processing(sclnt, dt_id)
 
 
