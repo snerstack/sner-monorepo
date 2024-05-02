86e4d05d9bc36a71a70e0a9ac72f6d686cfb5a53
Radoslav Bodo <bodik@cesnet.cz>
2024-05-02 12:42:41 +0000

    frontend: vitest, drop env cleanup hooks (not used?)

--- frontend/src/tests/setup.ts
+++ frontend/src/tests/setup.ts
@@ -3,14 +3,6 @@ import '@testing-library/jest-dom'
 import * as matchers from '@testing-library/jest-dom/matchers'
 import { cleanup } from '@testing-library/react'
 import { afterAll, afterEach, beforeAll, expect } from 'vitest'
-import { vi } from 'vitest'
-
-vi.stubEnv('VITE_SERVER_URL', 'http://localhost:18000')
-vi.stubEnv('VITE_HOST_TAGS', 'reviewed, todo')
-vi.stubEnv('VITE_SERVICE_TAGS', 'reviewed, todo')
-vi.stubEnv('VITE_VULN_TAGS', 'info, report, report:data, todo, falsepositive')
-vi.stubEnv('VITE_NOTE_TAGS', 'reviewed, todo')
-vi.stubEnv('VITE_ANNOTATE_TAGS', 'sslhell')
 
 expect.extend(matchers)
 
