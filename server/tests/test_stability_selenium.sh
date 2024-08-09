e899eaa674511d9d5541be515cf7609b05dc920e
Radoslav Bodó <bodik@cesnet.cz>
2024-08-09 15:17:19 +0200

    tests: updates stability tests

--- server/tests/test_stability_selenium.sh
+++ server/tests/test_stability_selenium.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 # This file is part of sner4 project governed by MIT license, see the LICENSE.txt file.
 #
-# test the stability of the tests
+# test the stability of the selenium tests
+export PYTEST_FRONTEND=build
 
-for iter in $(seq 1 100); do
-	timeout 600 make coverage
+for iter in $(seq 1 11); do
+	timeout 900 make test-selenium
 	RET=$?
 	echo "INFO: round ${iter}"
-	if [ $RET -eq 124 ]; then
-		echo "ERROR: timedout"
-		exit
-	fi
 	if [ $RET -ne 0 ]; then
 		echo "ERROR: error"
 		exit
