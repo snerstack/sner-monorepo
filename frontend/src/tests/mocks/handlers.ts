2431d483cd204e60964c1aba31118e493587e445
Radoslav Bodó <bodik@cesnet.cz>
2024-09-27 14:37:40 +0200

    frontend: resort mocks imports, reference with @/path

--- frontend/src/tests/mocks/handlers.ts
+++ frontend/src/tests/mocks/handlers.ts
@@ -1,39 +1,39 @@
-import { logoutHandler } from './handlers/auth/logout'
-import { webauthnListHandler } from './handlers/auth/profile/webauthn/list'
-import { userListHandler } from './handlers/auth/user/list'
-import { jobListHandler } from './handlers/scheduler/job/list'
-import { queueListHandler } from './handlers/scheduler/queue/list'
-import { hostListHandler } from './handlers/storage/host/list'
-import { hostLookupHandler } from './handlers/storage/host/lookup'
-import { noteGroupedHandler } from './handlers/storage/note/grouped'
-import { noteListHandler } from './handlers/storage/note/list'
-import { quickjumpHandler } from './handlers/storage/quickjump_autocomplete'
-import { serviceGroupedHandler } from './handlers/storage/service/grouped'
-import { serviceListHandler } from './handlers/storage/service/list'
-import { versioninfoListHandler } from './handlers/storage/versioninfo/list'
-import { vulnGroupedHandler } from './handlers/storage/vuln/grouped'
-import { vulnListHandler } from './handlers/storage/vuln/list'
-import { multicopyEndpointsHandler } from './handlers/storage/vuln/multicopy_endpoints'
-import { vulnSearchListHandler } from './handlers/storage/vulnsearch/list'
-import { dnsTreeHandler } from './handlers/visuals/dnstree'
+import { logoutHandler } from '@/tests/mocks/handlers/auth/logout'
+import { webauthnListHandler } from '@/tests/mocks/handlers/auth/profile/webauthn/list'
+import { userListHandler } from '@/tests/mocks/handlers/auth/user/list'
+import { jobListHandler } from '@/tests/mocks/handlers/scheduler/job/list'
+import { queueListHandler } from '@/tests/mocks/handlers/scheduler/queue/list'
+import { hostListHandler } from '@/tests/mocks/handlers/storage/host/list'
+import { hostLookupHandler } from '@/tests/mocks/handlers/storage/host/lookup'
+import { noteGroupedHandler } from '@/tests/mocks/handlers/storage/note/grouped'
+import { noteListHandler } from '@/tests/mocks/handlers/storage/note/list'
+import { quickjumpHandler } from '@/tests/mocks/handlers/storage/quickjump_autocomplete'
+import { serviceGroupedHandler } from '@/tests/mocks/handlers/storage/service/grouped'
+import { serviceListHandler } from '@/tests/mocks/handlers/storage/service/list'
+import { versioninfoListHandler } from '@/tests/mocks/handlers/storage/versioninfo/list'
+import { vulnGroupedHandler } from '@/tests/mocks/handlers/storage/vuln/grouped'
+import { vulnListHandler } from '@/tests/mocks/handlers/storage/vuln/list'
+import { multicopyEndpointsHandler } from '@/tests/mocks/handlers/storage/vuln/multicopy_endpoints'
+import { vulnSearchListHandler } from '@/tests/mocks/handlers/storage/vulnsearch/list'
+import { dnsTreeHandler } from '@/tests/mocks/handlers/visuals/dnstree'
 
 export const handlers = [
-  jobListHandler,
-  queueListHandler,
-  userListHandler,
-  webauthnListHandler,
+  dnsTreeHandler,
   hostListHandler,
   hostLookupHandler,
-  serviceListHandler,
-  serviceGroupedHandler,
-  vulnListHandler,
-  vulnGroupedHandler,
-  multicopyEndpointsHandler,
-  noteListHandler,
-  noteGroupedHandler,
-  versioninfoListHandler,
-  vulnSearchListHandler,
-  dnsTreeHandler,
+  jobListHandler,
   logoutHandler,
+  multicopyEndpointsHandler,
+  noteGroupedHandler,
+  noteListHandler,
+  queueListHandler,
   quickjumpHandler,
+  serviceGroupedHandler,
+  serviceListHandler,
+  userListHandler,
+  versioninfoListHandler,
+  vulnGroupedHandler,
+  vulnListHandler,
+  vulnSearchListHandler,
+  webauthnListHandler,
 ]
--- frontend/src/tests/mocks/server.ts
+++ frontend/src/tests/mocks/server.ts
@@ -1,5 +1,6 @@
-import { handlers } from './handlers'
 import { setupServer } from 'msw/node'
+
+import { handlers } from '@/tests/mocks/handlers'
 
 // This configures a Service Worker with the given request handlers.
 export const server = setupServer(...handlers)
