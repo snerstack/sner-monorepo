0e45e70f09ca39c382ecc440493853c32af75d6f
Radoslav Bodó <bodik@cesnet.cz>
2024-09-29 20:35:45 +0200

    plugin: nuclei, bump nuclei to 3.x
    https://github.com/snerstack/sner-monorepo/issues/87

--- server/bin/install_nuclei.sh
+++ server/bin/install_nuclei.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-wget --no-verbose -O /tmp/nuclei.zip https://github.com/projectdiscovery/nuclei/releases/download/v2.9.8/nuclei_2.9.8_linux_amd64.zip
+wget --no-verbose -O /tmp/nuclei.zip https://github.com/projectdiscovery/nuclei/releases/download/v3.3.4/nuclei_3.3.4_linux_amd64.zip
 unzip /tmp/nuclei.zip -d /opt/nuclei
 ln -sf /opt/nuclei/nuclei /usr/local/bin/nuclei
