8e912e7e906653ceb21e4799ab39372d866255e1
Radoslav Bodó <bodik@cesnet.cz>
2024-10-25 16:28:03 +0200

    agent: fix testssl installation (add more dependencies), bump testssl to the bleeding edge

--- server/bin/install_testssl.sh
+++ server/bin/install_testssl.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-git clone --depth 1 --branch v3.0.8 https://github.com/drwetter/testssl.sh.git /opt/testssl.sh
+git clone --depth 1 --branch v3.2rc3 https://github.com/drwetter/testssl.sh.git /opt/testssl.sh
 ln -sf /opt/testssl.sh/testssl.sh /usr/local/bin/testssl.sh
+
+# required by testssl
+apt-get install -y bind9-dnsutils bsdextrautils ldnsutils socat
 
 # required by parser
 apt-get install -y openssl
