a3fa9b9a0b040232870396384465c5ec0f3cc26c
Radoslav Bodó <bodik@cesnet.cz>
2024-10-28 11:18:16 +0100

    plugin: testssl, handle exit codes with grace, non-zero exist does not indicate complete fail

--- server/sner/plugin/testssl/agent.py
+++ server/sner/plugin/testssl/agent.py
@@ -27,6 +27,23 @@ class AgentModule(ModuleBase):  # pragma: cover-ignore-if-not-pytestslow
         super().__init__()
         self.loop = True
 
+    def _filter_exit_codes(self, value):  # pragma: nocover  ; won't test
+        """cope with testssl return values, not all !=0 values means job really failed
+        https://github.com/drwetter/testssl.sh/blob/3.2/doc/testssl.1.md#exit-status
+        """
+
+        if value < 200:
+            # testssl uses exit code as error counted, but the output can contain lots of good results
+            return 0
+        if value == 245:
+            # non-tls service, ipv4 vs ipv6, ...
+            return 0
+        if value == 246:
+            # connectivity issues
+            return 0
+
+        return value
+
     def run(self, assignment):
         """run the agent"""
 
@@ -40,11 +57,11 @@ class AgentModule(ModuleBase):  # pragma: cover-ignore-if-not-pytestslow
 
             target_args = ['--jsonfile-pretty', f'output-{idx}.json', f'{host}:{port}']
             cmd = ['testssl.sh', '--quiet', '--full', '-6', '--connect-timeout', '5', '--openssl-timeout', '5'] + target_args
-            ret |= self._execute(cmd, f'output-{idx}')
+            ret |= self._filter_exit_codes(self._execute(cmd, f'output-{idx}'))
 
             sleep(assignment['config']['delay'])
             if not self.loop:  # pragma: no cover  ; not tested
-                ret |= 2
+                ret = -16
                 break
 
         return ret
