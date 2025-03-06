7f33f7ad9d4d4005e4d3e6c1023c21feec9903c8
Radoslav Bodó <bodik@cesnet.cz>
2025-03-06 22:01:36 +0100

    frontend: add httpClient error handler, handle auth user edit error
    also https://github.com/snerstack/sner-monorepo/issues/63

--- frontend/src/lib/httpClient.ts
+++ frontend/src/lib/httpClient.ts
@@ -1,7 +1,25 @@
-import axios from 'axios'
-//import Cookies from 'js-cookie'
+import axios from "axios"
+import { toast } from "react-toastify"
 
 export const csrfTokenHeaderName = "X-CSRF-TOKEN"
+
+export const handleHttpClientError = (err: unknown) => {
+  if (axios.isAxiosError(err) && err.response?.data?.error) {
+    const { message, errors } = err.response.data.error
+
+    if (message) toast.error(message)
+
+    if (errors) {
+      Object.entries(errors).forEach(([field, messages]) => {
+        (Array.isArray(messages) ? messages : [messages]).forEach(msg =>
+          toast.error(`${field}: ${msg}`)
+        )
+      })
+    }
+  } else {
+    toast.error("An unexpected error occurred.")
+  }
+}
 
 const instance = axios.create({
   xsrfCookieName: "tokencsrf",
