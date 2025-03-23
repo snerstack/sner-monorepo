fd95b4d6ae58a98e522a4776cc2d3692d8552b6b
Radoslav Bodó <bodik@cesnet.cz>
2025-03-23 04:41:11 +0100

    server: bumps, tests, add test user_loader for flask3

--- server/tests/server/auth/test_core.py
+++ server/tests/server/auth/test_core.py
@@ -7,6 +7,10 @@ from http import HTTPStatus
 
 from flask import url_for
 
+from sner.server.password_supervisor import PasswordSupervisor as PWS
+from tests.server import get_csrf_token
+from tests.server.conftest import CustomTestApp
+
 
 def test_auth_session(cl_user):
     """test session auth"""
@@ -28,3 +32,24 @@ def test_auth_apikey(api_user):
 
     response = api_user.post_json(url_for('api.v2_public_storage_host_route'), {'address': '192.0.2.1'})
     assert response.status_code == HTTPStatus.OK
+
+
+def test_user_loader(app, user_factory):
+    """test user_loader"""
+    # flask3/flask-login uses g for tracking user authentication. while subsequent requests within
+    # single test app context "reuses user identity" from g, so user_loader is not triggered for coverage
+    # this test deliberately uses two separate contexts to test session based user_loader
+
+    client = CustomTestApp(app)
+    password = PWS.generate()
+    user = user_factory.create(password=PWS.hash(password))
+
+    with app.app_context():
+        form_data = [('username', user.username), ('password', password), ('csrf_token', get_csrf_token(client))]
+        response = client.post(url_for('auth.login_route'), params=form_data)
+        assert response.status_code == HTTPStatus.OK
+
+    with app.app_context():
+        response = client.get(url_for('auth.user_me_route'))
+        assert response.status_code == HTTPStatus.OK
+        assert response.json['username'] == user.username
