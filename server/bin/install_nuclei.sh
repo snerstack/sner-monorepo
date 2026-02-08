1f6207cb327355bca8b69691eea3a79185d888eb
Radoslav Bodó <bodik@cesnet.cz>
2026-02-08 10:26:29 +0100

    plugin: nuclei, bump version

--- server/bin/install_nuclei.sh
+++ server/bin/install_nuclei.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-wget --no-verbose -O /tmp/nuclei.zip https://github.com/projectdiscovery/nuclei/releases/download/v3.3.4/nuclei_3.3.4_linux_amd64.zip
+wget --no-verbose -O /tmp/nuclei.zip https://github.com/projectdiscovery/nuclei/releases/download/v3.6.2/nuclei_3.6.2_linux_amd64.zip
 unzip /tmp/nuclei.zip -d /opt/nuclei
 ln -sf /opt/nuclei/nuclei /usr/local/bin/nuclei
