1b871bb8cdb66a9fb2cc0c109d08b50e95de33f1
Radoslav Bodó <bodik@cesnet.cz>
2026-02-16 14:33:39 +0100

    plugin: remove ssh_audit, not used

--- server/bin/install_sshaudit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-mkdir /opt/ssh-audit
-cd /opt/ssh-audit
-python3 -m venv venv
-venv/bin/pip install ssh-audit
-
-ln -sf /opt/ssh-audit/venv/bin/ssh-audit /usr/local/bin/ssh-audit
