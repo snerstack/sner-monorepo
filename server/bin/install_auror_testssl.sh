c5be7e50cf04e46978ea98a5542b174f57151fb0
Radoslav Bodó <bodik@cesnet.cz>
2026-02-25 08:29:35 +0100

    plugin: auror_testssl, add procps dependency

--- server/bin/install_auror_testssl.sh
+++ server/bin/install_auror_testssl.sh
@@ -5,7 +5,7 @@ git clone --depth 1 --branch v3.2rc3 https://github.com/drwetter/testssl.sh.git 
 ln -sf /opt/testssl.sh/testssl.sh /usr/local/bin/testssl.sh
 
 # required by testssl
-apt-get install -y bind9-dnsutils bsdextrautils ldnsutils socat
+apt-get install -y bind9-dnsutils bsdextrautils ldnsutils procps socat
 
 # required by parser
 apt-get install -y openssl
