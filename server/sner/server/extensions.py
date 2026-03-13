992329e3ea6974b05b3156e09256418bbbedd47c
Radoslav Bodó <bodik@cesnet.cz>
2026-03-13 22:38:31 +0100

    server: review sner.server.sessions (fix, do not inject csrftoken cookie for API routes)
    https://github.com/snerstack/sner-monorepo/issues/99

--- server/sner/server/extensions.py
+++ server/sner/server/extensions.py
@@ -2,18 +2,23 @@
 """
 app extensions module
 """
+# pylint: disable=invalid-name
 
 from authlib.integrations.flask_client import OAuth
+from flask_cors import CORS
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_session import Session
 from flask_smorest import Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 
 from sner.server.wrapped_fido2_server import WrappedFido2Server
 
 api = Api()  # pylint: disable=invalid-name
 db = SQLAlchemy()  # pylint: disable=invalid-name
+cors = CORS()
+csrf = CSRFProtect()
 sess = Session()
 login_manager = LoginManager()  # pylint: disable=invalid-name
 migrate = Migrate()  # pylint: disable=invalid-name
